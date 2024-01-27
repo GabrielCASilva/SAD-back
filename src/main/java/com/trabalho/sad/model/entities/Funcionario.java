920c6a9bb54604cfca3f66be9d4f577122d24145
Victor-Malaga <victortolinimalaga@gmail.com>
2024-01-27 20:43:03 -0300

    Implementacao inicial de parte da camada de modelo do projeto SAD

--- src/main/java/com/trabalho/sad/model/entities/Funcionario.java
+++ src/main/java/com/trabalho/sad/model/entities/Funcionario.java
@@ -12,119 +12,198 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "funcionario")
 public class Funcionario {
-    // COMENTÁRIO TESTE-2
-    // COMENTÁRIO TESTE-3
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Long id;
-    @Column(name = "nome")
-    private String nome;
-    @Column(name = "dataNascimento")
-    private LocalDate dataNascimento;
-    @Column(name = "sexo")
-    private char sexo;
-    @Column(name = "cpf")
-    private String cpf;
+	
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "nome", nullable = false)
+	private String nome;
+	
+	@Column(name = "dataNascimento", nullable = false)
+	private LocalDate dataNascimento;
+	
+	@Column(name = "sexo", nullable = false)
+	private char sexo;
+	
+	@Column(name = "cpf", nullable = false)
+	private Long cpf;
+	
+	@Column(name = "cep", nullable = false)
+	private Long cep;
+	
+	@Column(name = "endereco", nullable = false)
+	private String endereco;
+	
+	@Column(name = "telefone", nullable = false)
+	private Long telefone;
+	
+	//@Column(name = "foto", nullable = false)
+	//private String foto;
+	
+	@Column(name = "email", nullable = false)
+	private String email;
+	
+	@Column(name = "dataCadastro", nullable = false)
+	private LocalDate dataCadastro;
+	
+	@Column(name = "situacao", nullable = false)
+	private String situacao;
+	
+	@Column(name = "login", nullable = false)
+	private String login;
+	
+	@Column(name = "senhaHash", nullable = false)
+	private String senhaHash;
 
-    public Funcionario(Long id, String nome, LocalDate dataNascimento, char sexo, String cpf) {
-        this.id = id;
-        this.nome = nome;
-        this.dataNascimento = dataNascimento;
-        this.sexo = sexo;
-        this.cpf = cpf;
-    }
+	
+	public Funcionario(Long id, String nome, LocalDate dataNascimento, char sexo, Long cpf, Long cep, String endereco,
+			Long telefone, String email, LocalDate dataCadastro, String situacao, String login, String senhaHash) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.dataNascimento = dataNascimento;
+		this.sexo = sexo;
+		this.cpf = cpf;
+		this.cep = cep;
+		this.endereco = endereco;
+		this.telefone = telefone;
+		this.email = email;
+		this.dataCadastro = dataCadastro;
+		this.situacao = situacao;
+		this.login = login;
+		this.senhaHash = senhaHash;
+	}
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public String getNome() {
-        return nome;
-    }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public LocalDate getDataNascimento() {
-        return dataNascimento;
-    }
 
-    public void setDataNascimento(LocalDate dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    public char getSexo() {
-        return sexo;
-    }
 
-    public void setSexo(char sexo) {
-        this.sexo = sexo;
-    }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-    public String getCpf() {
-        return cpf;
-    }
-    
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
-        result = prime * result + ((dataNascimento == null) ? 0 : dataNascimento.hashCode());
-        result = prime * result + sexo;
-        result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
-        return result;
-    }
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Funcionario other = (Funcionario) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        if (nome == null) {
-            if (other.nome != null)
-                return false;
-        } else if (!nome.equals(other.nome))
-            return false;
-        if (dataNascimento == null) {
-            if (other.dataNascimento != null)
-                return false;
-        } else if (!dataNascimento.equals(other.dataNascimento))
-            return false;
-        if (sexo != other.sexo)
-            return false;
-        if (cpf == null) {
-            if (other.cpf != null)
-                return false;
-        } else if (!cpf.equals(other.cpf))
-            return false;
-        return true;
-    }
 
-    @Override
-    public String toString() {
-        return "Funcionario [id=" + id + ", nome=" + nome + ", dataNascimento=" + dataNascimento + ", sexo=" + sexo
-                + ", cpf=" + cpf + "]";
-    }
-    
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+
+	public char getSexo() {
+		return sexo;
+	}
+
+
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+
+
+	public Long getCpf() {
+		return cpf;
+	}
+
+
+	public void setCpf(Long cpf) {
+		this.cpf = cpf;
+	}
+
+
+	public Long getCep() {
+		return cep;
+	}
+
+
+	public void setCep(Long cep) {
+		this.cep = cep;
+	}
+
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+
+	public Long getTelefone() {
+		return telefone;
+	}
+
+
+	public void setTelefone(Long telefone) {
+		this.telefone = telefone;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public LocalDate getDataCadastro() {
+		return dataCadastro;
+	}
+
+
+	public void setDataCadastro(LocalDate dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+
+	public String getSituacao() {
+		return situacao;
+	}
+
+
+	public void setSituacao(String situacao) {
+		this.situacao = situacao;
+	}
+
+
+	public String getLogin() {
+		return login;
+	}
+
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+
+	public String getSenhaHash() {
+		return senhaHash;
+	}
+
+
+	public void setSenhaHash(String senhaHash) {
+		this.senhaHash = senhaHash;
+	}
+	
 }
