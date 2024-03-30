e3655adbf3c0582b8fe20e760740a1e905cf6a3a
GabrielCASilva <gabriel.foxcarvalho@gmail.com>
2024-03-30 11:07:55 -0300

    juntando calculo com a requisição das avaliações

--- src/main/java/com/trabalho/sad/api/controller/TarefaController.java
+++ src/main/java/com/trabalho/sad/api/controller/TarefaController.java
@@ -16,10 +16,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.trabalho.sad.api.dto.AvaliacaoDTO;
 import com.trabalho.sad.api.dto.TarefaDTO;
 import com.trabalho.sad.exceptions.RegraNegocioException;
+import com.trabalho.sad.model.entities.AvaliacaoExecutor;
+import com.trabalho.sad.model.entities.AvaliacaoSuperior;
 import com.trabalho.sad.model.entities.Funcionario;
 import com.trabalho.sad.model.entities.Meta;
+import com.trabalho.sad.model.entities.ProgressoMeta;
 import com.trabalho.sad.model.entities.Tarefa;
 import com.trabalho.sad.service.FuncionarioService;
 import com.trabalho.sad.service.MetaService;
@@ -83,33 +87,50 @@ public class TarefaController {
 	}
 	
     @GetMapping(value = "/servidor/{servidorId}")
-    public ResponseEntity<List<Tarefa>> obterTarefasServidorPorPeriodo(
+    public ResponseEntity<AvaliacaoDTO> obterTarefasServidorPorPeriodo(
             @PathVariable Long servidorId,
             @RequestParam("dataInicio") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dataInicio,
             @RequestParam("dataFim") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dataFim) {
         Funcionario servidor = funcionarioService.consultarPorId(servidorId);
-        
         List<Tarefa> tarefas = tarefaService.obterTarefasServidorPorPeriodo(servidor, dataInicio, dataFim);
-        return ResponseEntity.ok(tarefas);
+
+		AvaliacaoExecutor avaliacao = new AvaliacaoExecutor(servidor, tarefas);
+		avaliacao.calcularAvaliacao();
+		AvaliacaoDTO avaliacaoDTO = new AvaliacaoDTO(avaliacao.getIndiceProdutividade(), 
+			avaliacao.getIndiceAgilidade(), avaliacao.getIndiceEficacia(), avaliacao.getIndiceComprometimento(), tarefas);
+
+        return ResponseEntity.ok(avaliacaoDTO);
     }
     
     @GetMapping(value = "/supervisor/{supervisorId}")
-    public ResponseEntity<List<Tarefa>> obterTarefasSupervisorPorPeriodo(
+    public ResponseEntity<AvaliacaoDTO> obterTarefasSupervisorPorPeriodo(
             @PathVariable Long supervisorId,
             @RequestParam("dataInicio") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dataInicio,
             @RequestParam("dataFim") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dataFim) {
         Funcionario supervisor = funcionarioService.consultarPorId(supervisorId);           
         List<Tarefa> tarefas = tarefaService.obterTarefasSupervisorPorPeriodo(supervisor, dataInicio, dataFim);
-        return ResponseEntity.ok(tarefas);
+
+		AvaliacaoSuperior avaliacao = new AvaliacaoSuperior(supervisor, tarefas);
+		avaliacao.calcularAvaliacao();
+		AvaliacaoDTO avaliacaoDTO = new AvaliacaoDTO(avaliacao.getIndiceProdutividade(), 
+			avaliacao.getIndiceAgilidade(), avaliacao.getIndiceEficacia(), avaliacao.getIndiceComprometimento(), tarefas);
+
+        return ResponseEntity.ok(avaliacaoDTO);
     }
     
     @GetMapping(value = "/meta/{metaId}")
-    public ResponseEntity<List<Tarefa>> obterTarefasDaMetaNoPeriodo(
+    public ResponseEntity<AvaliacaoDTO> obterTarefasDaMetaNoPeriodo(
             @PathVariable Long metaId,
             @RequestParam("dataInicio") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dataInicio,
             @RequestParam("dataFim") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dataFim) {
         Meta meta = metaService.consultarPorId(metaId);           
         List<Tarefa> tarefas = tarefaService.obterTarefasDaMetaNoPeriodo(meta, dataInicio, dataFim);
-        return ResponseEntity.ok(tarefas);
+
+		ProgressoMeta progresso = new ProgressoMeta(meta, tarefas);
+		progresso.calcularAvaliacao();
+		AvaliacaoDTO avaliacaoDTO = new AvaliacaoDTO(progresso.getIndiceProdutividade(), 
+			progresso.getIndiceAgilidade(), progresso.getIndiceEficacia(), tarefas);
+			
+        return ResponseEntity.ok(avaliacaoDTO);
     }
 }
--- src/main/java/com/trabalho/sad/api/dto/AvaliacaoDTO.java
+++ src/main/java/com/trabalho/sad/api/dto/AvaliacaoDTO.java
@@ -1,5 +1,76 @@
 package com.trabalho.sad.api.dto;
 
+import java.util.List;
+
+import com.trabalho.sad.model.entities.Tarefa;
+
 public class AvaliacaoDTO {
-    
+	private double indiceProdutividade;
+	private double indiceAgilidade;
+	private double indiceEficacia;
+	private double indiceComprometimento;
+    private List<Tarefa> tarefasAvaliacao = null; 
+
+    public AvaliacaoDTO(){
+
+    }
+
+    public AvaliacaoDTO(double indiceProdutividade, double indiceAgilidade, double indiceEficacia,
+            double indiceComprometimento, List<Tarefa> tarefasAvaliacao) {
+        this.indiceProdutividade = indiceProdutividade;
+        this.indiceAgilidade = indiceAgilidade;
+        this.indiceEficacia = indiceEficacia;
+        this.indiceComprometimento = indiceComprometimento;
+        this.tarefasAvaliacao = tarefasAvaliacao;
+    }
+
+    public AvaliacaoDTO(double indiceProdutividade, double indiceAgilidade, double indiceEficacia, 
+            List<Tarefa> tarefasAvaliacao) {
+        this.indiceProdutividade = indiceProdutividade;
+        this.indiceAgilidade = indiceAgilidade;
+        this.indiceEficacia = indiceEficacia;
+        this.tarefasAvaliacao = tarefasAvaliacao;
+    }
+
+    public double getIndiceProdutividade() {
+        return indiceProdutividade;
+    }
+
+    public void setIndiceProdutividade(double indiceProdutividade) {
+        this.indiceProdutividade = indiceProdutividade;
+    }
+
+    public double getIndiceAgilidade() {
+        return indiceAgilidade;
+    }
+
+    public void setIndiceAgilidade(double indiceAgilidade) {
+        this.indiceAgilidade = indiceAgilidade;
+    }
+
+    public double getIndiceEficacia() {
+        return indiceEficacia;
+    }
+
+    public void setIndiceEficacia(double indiceEficacia) {
+        this.indiceEficacia = indiceEficacia;
+    }
+
+    public double getIndiceComprometimento() {
+        return indiceComprometimento;
+    }
+
+    public void setIndiceComprometimento(double indiceComprometimento) {
+        this.indiceComprometimento = indiceComprometimento;
+    }
+
+    public List<Tarefa> getTarefasAvaliacao() {
+        return tarefasAvaliacao;
+    }
+
+    public void setTarefasAvaliacao(List<Tarefa> tarefasAvaliacao) {
+        this.tarefasAvaliacao = tarefasAvaliacao;
+    }
+
+
 }
